c428c503b78f24bc3662e0e27e26dcd3a3f1f87a
samakinpelu2008-dot <samakinpelu2008@gmail.com>
2026-06-26 21:40:08 +0100

    Implement asset caching in service worker
    Added caching for assets in the service worker.

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,20 @@
-self.addEventListener("install", () => {
-  console.log("App Installed");
+const CACHE_NAME = 'syrix-lab-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/images/IMG-20260626-WA0122.jpg', // Your main background
+  'https://unpkg.com/lucide@latest' // Icons
+];
+
+// Install the Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+// Serve assets from cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
 });
